6fef4e3c042f16fc1611724550b376802290b734
U-GOOGLE\zijiehe <zijiehe@chromium.org>
2016-06-29 18:26:27 -0700

    Update gn-debug / gn-release to use x86

--- chromium/gn-debug.cmd
+++ chromium/gn-debug.cmd
@@ -1,1 +1,1 @@
-gn gen out/GNDebug --args="is_debug=true is_component_build=true symbol_level=1 is_chrome_branded=true"
+gn gen out/GNDebug --args="is_debug=true is_component_build=true symbol_level=1 is_chrome_branded=true target_cpu=\"x86\""
