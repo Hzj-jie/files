a80492e2cd7443b532173c7335c056cc9eb7a84f
Zijie He <zijiehe@chromium.org>
2016-10-11 12:59:31 -0700

    Add gn x64

---
+++ chromium/gn-debug-x64.cmd
@@ -0,0 +1,1 @@
+gn gen out/GNDebug64 --args="is_debug=true is_component_build=false symbol_level=1 is_chrome_branded=true target_cpu=\"x64\""
---
+++ chromium/gn-release-x64.cmd
@@ -0,0 +1,1 @@
+gn gen out/GNRelease64 --args="is_debug=false is_component_build=false symbol_level=1 dcheck_always_on=true is_chrome_branded=true target_cpu=\"x64\""
