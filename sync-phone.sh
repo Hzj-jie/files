8d7f6a3914e9a126bc9a631bdd4c921babfac3e1
Zijie He <zijiehe@chromium.org>
2016-11-28 15:59:56 -0800

    Add sync-phone.sh

---
+++ sync-phone.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rsync.exe -avz -e "ssh -p 2222" hzj_jie@192.168.42.129:/storage/emulated/0/ /cygdrive/c/Users/zijiehe/temp/huawei-honor-6p-internal/
+rsync.exe -avz -e "ssh -p 2222" hzj_jie@192.168.42.129:/storage/C3A2-C39A/ /cygdrive/c/Users/zijiehe/temp/phone-sd/
