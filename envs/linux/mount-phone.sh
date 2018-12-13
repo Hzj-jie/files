e0f054862e29204375891a6433c32e45892fd87e
Zijie He <hzj_jie@hotmail.com>
2018-12-12 23:59:28 -0800

    mount-phone

--- envs/linux/mount-phone.sh
+++ envs/linux/mount-phone.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo sshhelper | sshfs -p 2222 -o password_stdin hzj_jie@192.168.43.1:/storage/ ~/phone/
+echo sshhelper | sshfs -p 2222 -o password_stdin hzj_jie@192.168.1.29:/storage/ ~/phone/
