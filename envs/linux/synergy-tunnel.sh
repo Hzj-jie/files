d29374ee0775f6bb24bc3d33f7ba25f2de450d2d
Zijie He <hzj_jie@hotmail.com>
2019-06-25 09:47:48 -0700

    continually run

--- envs/linux/synergy-tunnel.sh
+++ envs/linux/synergy-tunnel.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-ssh -N2qx -T -L 24800:localhost:24800 -oControlMaster=no -oPasswordAuthentication=no -okBdInteractiveAuthentication=no -oPubkeyAuthentication=yes zijiehe.kir.corp.google.com
+for ((;;))
+do
+  ssh -N2qx -T -L 24800:localhost:24800 -oControlMaster=no -oPasswordAuthentication=no -okBdInteractiveAuthentication=no -oPubkeyAuthentication=yes zijiehe.kir.corp.google.com
+done
