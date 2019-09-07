f5dfc5239a83a2b79db3a2e297de4fcff43fec40
Hzj_jie <hzj_jie@hotmail.com>
2019-09-06 23:41:27 -0700

    Add com.huawei.systemmanager

--- p10/disable-apps.sh
+++ p10/disable-apps.sh
@@ -3,12 +3,12 @@
 for line in `cat disabled-apps.txt`
 do
   adb shell pm disable-user --user 0 $line || true
-  adb shell pm disable-user --user 10 $line || true
+  adb shell pm disable-user --user 11 $line || true
   adb shell pm disable $line || true
   adb shell pm hide --user 0 $line || true
-  adb shell pm hide --user 10 $line || true
+  adb shell pm hide --user 11 $line || true
   adb shell pm hide $line || true
 done
 
-adb shell pm enable --user 10 com.google.android.gm || true
-adb shell pm unhide --user 10 com.google.android.gm || true
+adb shell pm enable --user 11 com.google.android.gm || true
+adb shell pm unhide --user 11 com.google.android.gm || true
