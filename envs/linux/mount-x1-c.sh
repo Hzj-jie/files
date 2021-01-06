f58b8083daffecfbf7d3f49767d75a6c128f4437
Hzj_jie <hzj_jie@hotmail.com>
2021-01-06 02:43:42 -0800

    update mount-x1-c.sh

--- envs/linux/mount-x1-c.sh
+++ envs/linux/mount-x1-c.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mount -t cifs -o user=Administrator //hzj-jie-x1/c$ /home/hzj_jie/x1/
+mount -t cifs -o user=Administrator,file_mode=0777,dir_mode=0777,iocharset=utf8 //hzj-jie-x1/c$ /home/hzj_jie/x1/
