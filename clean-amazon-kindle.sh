ba7d39efd49781e6c7d4e6299753cdd260b5d6fe
Hzj_jie <Hzj_jie@hotmail.com>
2021-05-16 02:13:37 -0700

    Do not expand file name in find

--- clean-amazon-kindle.sh
+++ clean-amazon-kindle.sh
@@ -2,4 +2,4 @@
 
 find /mnt/us/documents -name '*.mbs' -delete
 find /mnt/us/documents -name '*.mbp1' -delete
-find /mnt/us/documents -type d -name *.sdr -exec rmdir {} 2>/dev/null \;
+find /mnt/us/documents -type d -name '*.sdr' -exec rmdir {} 2>/dev/null \;
