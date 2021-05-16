8c86e4222d202c783253531a2cca3b50780e3025
Hzj_jie <Hzj_jie@hotmail.com>
2021-05-16 02:19:01 -0700

    Add pas pdt files in clean amazon kindle

--- clean-amazon-kindle.sh
+++ clean-amazon-kindle.sh
@@ -2,4 +2,7 @@
 
 find /mnt/us/documents -name '*.mbs' -delete
 find /mnt/us/documents -name '*.mbp1' -delete
+find /mnt/us/documents -name '*.pds' -delete
+find /mnt/us/documents -name '*.pdt' -delete
+
 find /mnt/us/documents -type d -name '*.sdr' -exec rmdir {} 2>/dev/null \;
