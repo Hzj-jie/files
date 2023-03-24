b41bcaa4234ea2e2aa490913c5db60d21a7c0f96
Hzj_jie <hzj_jie@hotmail.com>
2023-03-23 23:44:40 -0700

    build all

--- evox/evoscan/2.9/GaugeLayouts/serialized/build.sh
+++ evox/evoscan/2.9/GaugeLayouts/serialized/build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-cat $1.before.xml > $1.xml
-recode ascii..html < $1.content.xml >> $1.xml
-cat $1.after.xml >> $1.xml
+build_one() {
+  cat $1.before.xml > $1.xml
+  recode ascii..html < $1.content.xml >> $1.xml
+  cat $1.after.xml >> $1.xml
 
-mv $1.xml ../
+  mv $1.xml ../
+}
+
+for i in $(ls -1 *.content.xml) ; do
+  x=$(echo $i | sed 's/.content.xml//')
+  build_one $x
+done
