05d93cc259996d575f9cae40d921d2241a97801f
Hzj_jie <hzj_jie@hotmail.com>
2023-03-24 22:04:00 -0700

    Rename xml to xml.part

--- evox/evoscan/2.9/GaugeLayouts/serialized/build.sh
+++ evox/evoscan/2.9/GaugeLayouts/serialized/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 build_one() {
-  cat $1.before.xml > $1.xml
-  recode ascii..html < $1.content.xml >> $1.xml
-  cat $1.after.xml >> $1.xml
+  cat $1.before.xml.part > $1.xml
+  recode ascii..html < $1.content.xml.part >> $1.xml
+  cat $1.after.xml.part >> $1.xml
 
   mv $1.xml ../
 }
 
-for i in $(ls -1 *.content.xml) ; do
-  x=$(echo $i | sed 's/.content.xml//')
+for i in $(ls -1 *.content.xml.part) ; do
+  x=$(echo $i | sed 's/.content.xml.part//')
   build_one $x
 done
