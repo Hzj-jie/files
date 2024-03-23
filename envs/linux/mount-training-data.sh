48400a2bba62a761d70c1b835ff65d2d66915913
Zijie He <hzj_jie@hotmail.com>
2024-03-22 17:44:15 -0700

    username

--- envs/linux/mount-training-data.sh
+++ envs/linux/mount-training-data.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mount -t cifs -o username=guest //192.168.122.71/training-data /home/zijiehe/training-data
+mount -t cifs -o username=hzj_j //192.168.122.71/training-data /home/zijiehe/training-data
